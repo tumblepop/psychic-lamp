febed4a1129f73b11c03fcad6d3b43c06b4142db
Jimmy <wwwjjj13@126.com>
2018-03-21 19:05:06 +0800

    challenge 1

--- calculator.py
+++ calculator.py
@@ -1,5 +1,24 @@
 #!/usr/bin/env python3
 
 import sys
-GongZiJinE = sys.argv[1]
-print(GongZiJinE)
+try:
+    GongZiJinE = int(float(sys.argv[1]))
+except:
+    print("Parameter Error")
+
+YiNaShuiE = GongZiJinE - 3500
+if YiNaShuiE <= 1500:
+    tax = YiNaShuiE * 0.03
+elif YiNaShuiE > 1500 and YiNaShuiE <= 4500:
+    tax = YiNaShuiE * 0.10 - 105
+elif YiNaShuiE > 4500 and YiNaShuiE <= 9000:
+    tax = YiNaShuiE * 0.20 - 555
+elif YiNaShuiE > 9000 and YiNaShuiE <= 35000:
+    tax = YiNaShuiE * 0.25 - 1005
+elif YiNaShuiE > 35000 and YiNaShuiE <= 55000:
+    tax = YiNaShuiE * 0.30 - 2755
+elif YiNaShuiE > 55000 and YiNaShuiE <= 80000:
+    tax = YiNaShuiE * 0.35 - 5505
+elif YiNaShuiE > 80000:
+    tax = YiNaShuiE * 0.45 - 13505
+print("{:.2f}".format(tax)) 
